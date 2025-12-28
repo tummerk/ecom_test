da774911168fccc0911ee22550b6f13c7919269b
tummerk <96657150+tummerk@users.noreply.github.com>
2025-12-28 23:10:25 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import (
 	"ecom_test/internal/config"
 )
 
-func main() {
+func main() { //понятно что конфиг надо задавать в env, но т.к. сторонние либы нельзя было использовать сделал просто внутри для упрощения надеюсь не сильно плохо 
 	cfg := config.Config{
 		Addr:            ":8080",
 		ShutdownTimeout: 10,
